35e5b1edeadb5e1321fb4dfe2f7370b65b660115
LinpluX <podtelkovs@bk.ru>
2021-11-12 21:43:41 +0800

    Найти третью цифру числа или сообщить, что её нет

---
+++ Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число больше 1 и меньше 1000");
+int a = Convert.ToInt32(Console.ReadLine());
+if ( a < 1 || a > 999 )
+{
+    Console.WriteLine("Введите число по выше предложенному диапазону");
+    return;
+}
+int SNumber (int Num)
+{
+    if(Num < 100)
+    {
+       return Num = Num % 10;
+    } 
+    else
+    {
+        return Num = Num / 10 % 10;
+
+    }
+}
+Console.WriteLine("Вторая цифра в числе равняется:");
+Console.WriteLine(SNumber(a));
+
+
---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int Chet(int num)
+{
+    if (num % 7 % 23 == 0)
+    {
+        Console.WriteLine("Ваше число кратно заданным делителям так как остаток :");
+        return num % 7 % 23;
+    }
+    else
+    {
+        
+        Console.WriteLine("Ваше число не кратно заданным делителям так как остаток:");
+        return num % 7 % 23;
+    }
+}
+Console.WriteLine(Chet(a));
